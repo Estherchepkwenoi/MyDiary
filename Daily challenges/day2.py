d71ebd8d517780daf0fb276ba697e854dbeb39da
Andela <chepkwenoiesther@gmail.com>
2018-09-06 16:54:09 +0300

    add changes to day2.py

--- Daily challenges/day2.py
+++ Daily challenges/day2.py
@@ -1,2 +1,17 @@
-Tuple=(aphabetletters)
-print(Tuple)
+def my_Vowels():
+    Vowels=['a,b,c,d,e']
+    count=0
+    vowl=""
+    string=str("Enter your string:")
+    for i in string:
+        for i in Vowels:
+            count+=1
+            vowl+=i
+            print(vowl,count)
+my_Vowels()            
+        
+
+
+            
+            
+            
